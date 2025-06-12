25520e1e10c1b44c175239003323e5f5fe050319
Iver <iver.mortensen@gmail.com>
2025-06-12 12:37:36 +0200

    Added caps lock toggle when both shift keys are tapped at the same time.

--- keymap.c
+++ keymap.c
@@ -15,6 +15,12 @@
  */
 
  #include QMK_KEYBOARD_H
+#include "timer.h"
+
+static bool lshift_tap = false;
+static bool rshift_tap = false;
+static uint16_t lshift_timer = 0;
+static uint16_t rshift_timer = 0;
 
  enum planck_layers { _QWERTY, _SYMB, _SYMB_SHIFT, _NAV, _FN, _NO, _MAC_QUERTY, _MAC_SYMB, _MAC_SYMB_SHIFT, _MAC_NAV, _MAC_FN, _MAC_NO };
 
@@ -127,200 +133,32 @@
      )
  };
 
-//  #ifdef ENCODER_MAP_ENABLE
-//  /* Rotary Encoders
-//   */
-//  const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-//      /* Qwerty
-//       *    v- (index) Clockwise / Counter Clockwise                        v- (index) Clockwise / Counter Clockwise
-//       * ,---------------------------------------------------------------------------------------.
-//       * | (0) Vol-    / Vol+    |   |   |   |   |   |   |   |   |   |   | (4) Vol-    / Vol+    |
-//       * |-----------------------+---+---+---+---+---+---+---+---+---+---+-----------------------|
-//       * | (1) KC_MNXT / KC_MPRV |   |   |   |   |   |   |   |   |   |   | (5) KC_MNXT / KC_MPRV |
-//       * |-----------------------+---+---+---+---+---+---+---+---+---+---+-----------------------|
-//       * | (2) KC_WBAK / KC_WFWD |   |   |   |   |   |   |   |   |   |   | (6) KC_SPC  / KC_ENT  |
-//       * |-----------------------+---+---+---+---+---+---+---+---+---+---+-----------------------|
-//       * | (3) KC_LEFT / KC_RGHT |   |   |   |   |       |   |   |   |   | (7) KC_DOWN / KC_UP   |
-//       * `---------------------------------------------------------------------------------------'
-//       */
-//      [_QWERTY] = {
-//          // LEFT SIDE (index 0 to 3)
-//          ENCODER_CCW_CW(KC_VOLU, KC_VOLD),
-//          ENCODER_CCW_CW(KC_MNXT, KC_MPRV),
-//          ENCODER_CCW_CW(KC_WBAK, KC_WFWD),
-//          ENCODER_CCW_CW(KC_LEFT, KC_RGHT),
-//          // RIGHT SIDE (index 4 to 7)
-//          ENCODER_CCW_CW(KC_VOLU, KC_VOLD),
-//          ENCODER_CCW_CW(KC_MNXT, KC_MPRV),
-//          ENCODER_CCW_CW(KC_SPC,  KC_ENT),
-//          ENCODER_CCW_CW(KC_DOWN, KC_UP)
-//      },
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (keycode == KC_LSFT) {
+        if (record->event.pressed) {
+            lshift_timer = timer_read();
+        } else {
+            lshift_tap = timer_elapsed(lshift_timer) < 200;
+            if (lshift_tap && rshift_tap && timer_elapsed(rshift_timer) < 200) {
+                tap_code(KC_CAPS);
+                lshift_tap = false;
+                rshift_tap = false;
+            }
+        }
+    }
 
-//      /* Adjust (Lower + Raise)
-//       *    v- (index) Clockwise / Counter Clockwise                        v- (index) Clockwise / Counter Clockwise
-//       * ,---------------------------------------------------------------------------------------.
-//       * | (0) _______ / _______ |   |   |   |   |   |   |   |   |   |   | (4) _______ / _______ |
-//       * |-----------------------+---+---+---+---+---+---+---+---+---+---+-----------------------|
-//       * | (1) _______ / _______ |   |   |   |   |   |   |   |   |   |   | (5) _______ / _______ |
-//       * |-----------------------+---+---+---+---+---+---+---+---+---+---+-----------------------|
-//       * | (2) UG_NEXT / UG_PREV |   |   |   |   |   |   |   |   |   |   | (6) SAT- / SAT+       |
-//       * |-----------------------+---+---+---+---+---+---+---+---+---+---+-----------------------|
-//       * | (3) UG_VALD / UG_VALU |   |   |   |   |       |   |   |   |   | (7) HUE- / HUE+       |
-//       * `---------------------------------------------------------------------------------------'
-//       */
-//      [_ADJUST] = {
-//          // LEFT SIDE (index 0 to 3)
-//          ENCODER_CCW_CW(_______, _______),
-//          ENCODER_CCW_CW(_______, _______),
-//          ENCODER_CCW_CW(UG_NEXT, UG_PREV),
-//          ENCODER_CCW_CW(UG_VALD, UG_VALU),
-//          // RIGHT SIDE (index 4 to 7)
-//          ENCODER_CCW_CW(_______, _______),
-//          ENCODER_CCW_CW(_______, _______),
-//          ENCODER_CCW_CW(UG_SATD,  UG_SATU),
-//          ENCODER_CCW_CW(UG_HUEU,  UG_HUED)
-//      }
-//  };
-//  #endif
- /* clang-format on */
+    if (keycode == KC_RSFT) {
+        if (record->event.pressed) {
+            rshift_timer = timer_read();
+        } else {
+            rshift_tap = timer_elapsed(rshift_timer) < 200;
+            if (lshift_tap && rshift_tap && timer_elapsed(lshift_timer) < 200) {
+                tap_code(KC_CAPS);
+                lshift_tap = false;
+                rshift_tap = false;
+            }
+        }
+    }
 
-//  #ifdef AUDIO_ENABLE
-//  float plover_song[][2]    = SONG(PLOVER_SOUND);
-//  float plover_gb_song[][2] = SONG(PLOVER_GOODBYE_SOUND);
-//  #endif
-
-//  bool play_encoder_melody(uint8_t index, bool clockwise);
-
-//  layer_state_t layer_state_set_user(layer_state_t state) {
-//      return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-//  }
-
-//  bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-//  #ifdef ENCODER_MAP_ENABLE
-//      if (IS_ENCODEREVENT(record->event) && record->event.pressed) {
-//          play_encoder_melody(record->event.key.col, record->event.type == ENCODER_CCW_EVENT);
-//      }
-//  #endif
-//      switch (keycode) {
-//          case BACKLIT:
-//              if (record->event.pressed) {
-//                  register_code(KC_RSFT);
-//              } else {
-//                  unregister_code(KC_RSFT);
-//              }
-//              return false;
-//              break;
-//          case PLOVER:
-//              if (record->event.pressed) {
-//  #ifdef AUDIO_ENABLE
-//                  stop_all_notes();
-//                  PLAY_SONG(plover_song);
-//  #endif
-//                  layer_off(_RAISE);
-//                  layer_off(_LOWER);
-//                  layer_off(_ADJUST);
-//                  layer_on(_PLOVER);
-//                  if (!eeconfig_is_enabled()) {
-//                      eeconfig_init();
-//                  }
-//                  keymap_config.raw  = eeconfig_read_keymap();
-//                  keymap_config.nkro = 1;
-//                  eeconfig_update_keymap(keymap_config.raw);
-//              }
-//              return false;
-//              break;
-//          case EXT_PLV:
-//              if (record->event.pressed) {
-//  #ifdef AUDIO_ENABLE
-//                  PLAY_SONG(plover_gb_song);
-//  #endif
-//                  layer_off(_PLOVER);
-//              }
-//              return false;
-//              break;
-//      }
-//      return true;
-//  }
-
-//  /* clang-format off */
-//  float melody[8][2][2] = {
-//      {{440.0f, 8}, {440.0f, 24}},
-//      {{440.0f, 8}, {440.0f, 24}},
-//      {{440.0f, 8}, {440.0f, 24}},
-//      {{440.0f, 8}, {440.0f, 24}},
-//      {{440.0f, 8}, {440.0f, 24}},
-//      {{440.0f, 8}, {440.0f, 24}},
-//      {{440.0f, 8}, {440.0f, 24}},
-//      {{440.0f, 8}, {440.0f, 24}},
-//  };
-//  /* clang-format on */
-
-//  #define JUST_MINOR_THIRD 1.2
-//  #define JUST_MAJOR_THIRD 1.25
-//  #define JUST_PERFECT_FOURTH 1.33333333
-//  #define JUST_TRITONE 1.42222222
-//  #define JUST_PERFECT_FIFTH 1.33333333
-
-//  #define ET12_MINOR_SECOND 1.059463
-//  #define ET12_MAJOR_SECOND 1.122462
-//  #define ET12_MINOR_THIRD 1.189207
-//  #define ET12_MAJOR_THIRD 1.259921
-//  #define ET12_PERFECT_FOURTH 1.33484
-//  #define ET12_TRITONE 1.414214
-//  #define ET12_PERFECT_FIFTH 1.498307
-
-//  deferred_token tokens[8];
-
-//  uint32_t reset_note(uint32_t trigger_time, void *note) {
-//      *(float*)note = 440.0f;
-//      return 0;
-//  }
-
-//  bool play_encoder_melody(uint8_t index, bool clockwise) {
-//      cancel_deferred_exec(tokens[index]);
-//      if (clockwise) {
-//          melody[index][1][0] = melody[index][1][0] * ET12_MINOR_SECOND;
-//          melody[index][0][0] = melody[index][1][0] / ET12_PERFECT_FIFTH;
-//          audio_play_melody(&melody[index], 2, false);
-//      } else {
-//          melody[index][1][0] = melody[index][1][0] / ET12_MINOR_SECOND;
-//          melody[index][0][0] = melody[index][1][0] * ET12_TRITONE;
-//          audio_play_melody(&melody[index], 2, false);
-//      }
-//      tokens[index] = defer_exec(1000, reset_note, &melody[index][1][0]);
-//      return false;
-//  }
-
-//  bool encoder_update_user(uint8_t index, bool clockwise) {
-//      return play_encoder_melody(index, clockwise);
-//  }
-
-//  bool dip_switch_update_user(uint8_t index, bool active) {
-//      switch (index) {
-//          case 0: {
-//  #ifdef AUDIO_ENABLE
-//              static bool play_sound = false;
-//  #endif
-//              if (active) {
-//  #ifdef AUDIO_ENABLE
-//                  if (play_sound) {
-//                      PLAY_SONG(plover_song);
-//                  }
-//  #endif
-//                  layer_on(_ADJUST);
-//              } else {
-//  #ifdef AUDIO_ENABLE
-//                  if (play_sound) {
-//                      PLAY_SONG(plover_gb_song);
-//                  }
-//  #endif
-//                  layer_off(_ADJUST);
-//              }
-//  #ifdef AUDIO_ENABLE
-//              play_sound = true;
-//  #endif
-//              break;
-//          }
-//      }
-//      return true;
-//  }
+    return true;
+}
